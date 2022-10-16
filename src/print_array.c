4a2e49fbe623245c80f021dab612c1c4a85d5741
onyx <onyx@onyx.onyx>
2022-10-16 21:14:10 +0200

    print_array

---
+++ src/print_array.c
@@ -0,0 +1,18 @@
+#include "push_swap.h"
+
+#include <stdio.h>
+
+void	print_array(t_data *data)
+{
+	int i;
+
+	i = 0;
+	while (i < data->max_len)
+	{
+		printf(" %d\t| %d\n",
+			   data->stack_a[i] > 0 ? data->stack_a[i]: ' ',
+			   data->stack_b[i] > 0 ? data->stack_b[i]: ' ');
+		i++;
+	}
+	printf("A_\t|B_\n");
+}
